134d03f3b5810b92ebb21fafc3a376dd7c5e4d7a
Emily Black <emily.black@podium.com>
2022-04-04 15:12:53 -0600

    Not sure why I didn't push these

--- back-end/server.js
+++ back-end/server.js
@@ -14,5 +14,97 @@ mongoose.connect('mongodb://127.0.0.1:27017/wishlist', {
   useNewUrlParser: true
 });
 
+// Create a new item in the museum: takes a title and a path to an image.
+app.post('/api/items', async (req, res) => {
+  const item = new Item({
+    title: req.body.title,
+    path: req.body.path,
+    description: req.body.description,
+    price: req.body.price,
+    location: req.body.location
+  });
+  try {
+    await item.save();
+    res.send(item);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+const multer = require('multer')
+const upload = multer({
+  dest: '../front-end/public/images/',
+  limits: {
+    fileSize: 10000000
+  }
+});
+
+// Create a scheme for items in the museum: a title and a path to an image.
+const itemSchema = new mongoose.Schema({
+  title: String,
+  path: String,
+  description: String,
+  price: String,
+  location: String
+});
+
+// Create a model for items in the museum.
+const Item = mongoose.model('Item', itemSchema);
+
+app.post('/api/photos', upload.single('photo'), async (req, res) => {
+  // Just a safety check
+  if (!req.file) {
+    return res.sendStatus(400);
+  }
+  res.send({
+    path: "/images/" + req.file.filename
+  });
+});
+
+app.delete('/api/items/:id', async (req, res) => {
+  try {
+    await Item.deleteOne({
+      _id: req.params.id
+    });
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+app.put('/api/items/:id', async (req, res) => {
+  try {
+    let item = await Item.findOne({
+      _id: req.params.id
+
+    });
+    console.log(item)
+
+    item.title = req.body.title;
+    console.log(req.body.description);
+
+    item.description = req.body.description;
+    item.price = req.body.price;
+    item.location = req.body.location;
+
+    item.save();
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+app.get('/api/items', async (req, res) => {
+  try {
+    let items = await Item.find();
+    res.send(items);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
